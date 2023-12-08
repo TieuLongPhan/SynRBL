722850bc1d19ba4694cb9c178beb3c75f0821958
Tieu Long Phan <ptlong.chcndpbc20@ump.edu.vn>
2023-12-08 22:15:29 +0700

    update new python file

--- Notebook/test_find_graph.py
+++ Notebook/test_find_graph.py
@@ -10,18 +10,13 @@ from SynRBL.SynMCS.mcs_utils import smiles_to_mol_parallel
 from SynRBL.SynMCS.find_missing_graphs import FindMissingGraphs
 
 
-def main(save: bool = False, save_path: str = f'{root_dir}/Data/MCS/final_graph.json.gz'):
+def find_graph_dict(msc_dict_path: str,  save_path: str, save: bool =True,
+                    n_jobs: int=4, use_findMCS: bool=True):
     """
-    Main function to process data and find missing graphs.
-
-    Args:
-        save (bool, optional): Flag to indicate if the results should be saved. Defaults to False.
-        save_path (str, optional): File path to save the results. Defaults to f'{root_dir}/Data/MCS/final_graph.json.gz'.
-
-    Returns:
-        None
+    Function to find missing graphs for a given MCS dictionary.
     """
-    mcs_dict = load_database(f'{root_dir}/Data/MCS/Intersection_MCS.json.gz')
+    mcs_dict = load_database(msc_dict_path)
+
     msc_df = pd.DataFrame(mcs_dict)
 
     mcs_results = msc_df['mcs_results'].to_list()
@@ -31,11 +26,29 @@ def main(save: bool = False, save_path: str = f'{root_dir}/Data/MCS/final_graph.
     sorted_reactants_mol_list = smiles_to_mol_parallel(sorted_reactants)
 
     find_graph = FindMissingGraphs()
-    missing_results = find_graph.find_single_graph_parallel(mcs_mol_list, sorted_reactants_mol_list, n_jobs=4, use_findMCS=True)
+    missing_results = find_graph.find_single_graph_parallel(mcs_mol_list, sorted_reactants_mol_list, n_jobs=n_jobs, use_findMCS=use_findMCS)
+    missing_final = pd.DataFrame(missing_results)
 
+    missing_final = missing_final.map(lambda x: np.nan if x == '' else x)
+    non_pass_df = missing_final.iloc[missing_final.dropna(subset=['issue']).index,:].to_dict(orient='records')
+    print('Bug:', len(non_pass_df))
     if save:
         save_database(missing_results, save_path)
+    
+    return missing_results
+
+
+
+def main():
+
+    missing_results_3_macth = find_graph_dict(msc_dict_path=f'{root_dir}/Data/MCS/Intersection_MCS_3+_matching_ensemble.json.gz',
+                save_path=f'{root_dir}/Data/MCS/Final_Graph_macth_3+.json.gz')
+    
+    missing_results_largest = find_graph_dict(msc_dict_path=f'{root_dir}/Data/MCS/Intersection_MCS_0_50_largest.json.gz',
+                save_path=f'{root_dir}/Data/MCS/Final_Graph_macth_under2-.json.gz')
+    
+    
 
 # Execute main function
 if __name__ == "__main__":
-    main(save=True)
+    main()
