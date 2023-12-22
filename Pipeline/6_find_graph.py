e57e9714508c84f0ee413f0c8ced8541451de9bf
TieuLongPhan <ptlong.chcndpbc20@ump.edu.vn>
2023-12-22 13:20:27 +0100

    update refinement for uncertainty data

--- Pipeline/6_find_graph.py
+++ Pipeline/6_find_graph.py
@@ -4,11 +4,11 @@ root_dir = Path(__file__).parents[1]
 sys.path.append(str(root_dir))
 from SynRBL.rsmi_utils import load_database, save_database
 from SynRBL.SynMCS.MissingGraph.find_graph_dict import find_graph_dict
+from SynRBL.SynMCS.MissingGraph.refinement_uncertainty import RefinementUncertainty
 
 
 
-
-def main():
+def graph_find():
 
 
     data = load_database(f'{root_dir}/Data/Unsolved_reactions.json.gz')
@@ -27,9 +27,38 @@ def main():
             missing_results_largest[key]['old_reaction'] = data[key]['reactions']
         save_database(missing_results_largest, root_dir / f'Data/MCS/Final_graph_macth_condition_{i}.json.gz')
 
+def graph_refinment():
+    finalgraph = load_database(root_dir / 'Data/MCS/Final_Graph.json.gz')
+    finalgraph_certainty = [i for i in finalgraph if i['Certainty'] == True]
+    finalgraph_uncertainty = [i for i in finalgraph if i['Certainty'] == False]
+
+    graph_condition_1 = load_database(root_dir / f'Data/MCS/Final_graph_macth_condition_1.json.gz')
+    graph_condition_2 = load_database(root_dir / f'Data/MCS/Final_graph_macth_condition_2.json.gz')
+    graph_condition_3 = load_database(root_dir / f'Data/MCS/Final_graph_macth_condition_3.json.gz')
+    graph_condition_4 = load_database(root_dir / f'Data/MCS/Final_graph_macth_condition_4.json.gz')
+    #graph_condition_5 = load_database(root_dir / f'Data/MCS/Final_graph_macth_condition_5.json.gz')
+
+    graph_cond_list = [finalgraph, graph_condition_1, graph_condition_2, graph_condition_3, graph_condition_4]
+
+    refine = RefinementUncertainty(finalgraph_uncertainty, graph_cond_list)
+    new_graph = refine.fit(intersection_num=3)
+    new_certain = [new_graph[i] for i in range(len(new_graph)) if new_graph[i]['Certainty'] == True]
+    new_certain_id = [new_certain[i]['R-id'] for i in range(len(new_certain))]
+
+    finalgraph_certainty.extend(new_certain)
+
+    remove_list = [x for x in finalgraph_uncertainty if x['R-id'] in new_certain_id]
+    for i in remove_list:
+        finalgraph_uncertainty.remove(i) 
+
+    save_database(finalgraph_certainty, root_dir / 'Data/MCS/Final_Graph_certainty.json.gz')
+    save_database(finalgraph_uncertainty, root_dir / 'Data/MCS/Final_Graph_uncertainty.json.gz')
+
+    
     
     
 
 # Execute main function
 if __name__ == "__main__":
-    main()
+    #graph_find()
+    graph_refinment()
---
+++ SynRBL/SynMCS/MissingGraph/refinement_uncertainty.py
@@ -0,0 +1,76 @@
+from typing import List, Tuple, Set, Optional
+from collections import Counter
+class RefinementUncertainty:
+    def __init__(self, finalgraph_uncertainty, graph_conditions: List[dict]):
+        """
+        Initialize the RefinementUncertainty class with the graph conditions.
+
+        Parameters:
+        finalgraph_uncertainty (List[dict]): A list of dictionaries representing missing graph data.
+        graph_conditions (List[dict]): A list of dictionaries representing graph conditions.
+        """
+        self.finalgraph_uncertainty = finalgraph_uncertainty
+        self.id = [i['R-id'] for i in finalgraph_uncertainty]
+        self.graph_conditions = graph_conditions
+
+    @staticmethod
+    def get_smiles_graph(final_graph: List[dict], id: str) -> List[str]:
+        """
+        Retrieve the SMILES graph for a given final graph and ID.
+
+        Parameters:
+        final_graph (List[dict]): A list of dictionaries representing missing graph data.
+        id (str): The ID to search for in the graph.
+
+        Returns:
+        List[str]: The SMILES graph for the given ID.
+        """
+        return [item['smiles'] for item in final_graph if item['R-id'] == id][0]
+    
+    @staticmethod
+    def intersection_of_lists_with_count(lists: List[List[str]], intersection_num: int = 2) -> Tuple[Optional[List[str]], Optional[int]]:
+        """
+        Find lists that are present at least a specified number of times in the provided list of lists and their first index.
+
+        Parameters:
+        lists (List[List[str]]): A list containing multiple lists of elements.
+        intersection_num (int): The minimum number of times a list must appear to be considered.
+
+        Returns:
+        Tuple[Optional[List[str]], Optional[int]]: The first list that meets the intersection criteria and its first index, or None if no such list exists.
+        """
+        # Convert inner lists to tuples for hashing and count occurrences
+        tuple_lists = [tuple(lst) for lst in lists]
+        counts = Counter(tuple_lists)
+
+        # Find the first list that meets the intersection criteria
+        for index, lst in enumerate(tuple_lists):
+            if counts[lst] >= intersection_num:
+                return list(lst), index  # Convert tuple back to list for the result
+
+        return None, None
+
+    def fit(self, intersection_num: int = 2) -> List[dict]:
+        """
+        Generate a new graph for uncertain IDs based on the first intersection condition.
+
+        Parameters:
+        id_uncertainty (List[str]): A list of uncertain IDs.
+        intersection_num (int): The minimum number of lists in which an element must appear to be considered.
+
+        Returns:
+        List[dict]: A new graph list for uncertain IDs.
+        """
+        new_graph_uncertain = []
+        for id in self.id:
+            list_cond = [self.get_smiles_graph(cond, id) for cond in self.graph_conditions]
+            intersection, first_key_index = self.intersection_of_lists_with_count(list_cond, intersection_num)
+            if intersection and first_key_index is not None:
+                match_cond = self.graph_conditions[first_key_index]
+                new_graph = [value for value in match_cond if value['R-id'] == id]
+                new_graph_uncertain.extend(new_graph)
+            else:
+                new_graph = [value for value in self.finalgraph_uncertainty if value['R-id'] == id]
+                new_graph_uncertain.extend(new_graph)
+
+        return new_graph_uncertain
