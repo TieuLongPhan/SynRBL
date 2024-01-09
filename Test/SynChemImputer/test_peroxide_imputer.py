bbc5bda7beb4bcfc15e3c6a64277ffab175a30e4
TieuLongPhan <ptlong.chcndpbc20@ump.edu.vn>
2024-01-09 10:42:41 +0100

    update peroxid group imputer and test case

---
+++ Test/SynChemImputer/test_peroxide_imputer.py
@@ -0,0 +1,47 @@
+import unittest
+import sys
+from pathlib import Path
+root_dir = Path(__file__).parents[2]
+sys.path.append(str(root_dir))
+from SynRBL.SynChemImputer.peroxide_imputer import PeroxidGroupImputer  # Replace with the actual module name
+
+class TestPeroxidGroupImputer(unittest.TestCase):
+
+    def setUp(self):
+        self.pero_imputer = PeroxidGroupImputer()
+
+    def test_fix_peroxid_group(self):
+        # Test data and expected result for the first reaction
+        reactions_dict = [{'reactions': 'COOC.C>>C'}]
+        expected_result = {'reactions': 'COOC.C>>C.CO.CO'}  # Assuming this is the expected updated reaction
+
+        # Applying the fix method
+        self.pero_imputer.fix(reactions_dict[0], 'reactions')
+
+        # Assertion
+        self.assertEqual(reactions_dict[0], expected_result)
+
+    def test_fix_peracid_group(self):
+        # Test data and expected result for the second reaction
+        reactions_dict = [{'reactions': 'C=C.C1=CC(=CC(=C1)Cl)C(=O)OO>>O1CC1'}]
+        expected_result = {'reactions': 'C=C.C1=CC(=CC(=C1)Cl)C(=O)OO>>O1CC1.O=C(O)c1cccc(Cl)c1'}  # Updated reaction
+
+        # Applying the fix method
+        self.pero_imputer.fix(reactions_dict[0], 'reactions')
+
+        # Assertion
+        self.assertEqual(reactions_dict[0], expected_result)
+
+    def test_fix_peracid_group_not_impute(self):
+        # Test data and expected result for the third reaction
+        reactions_dict = [{'reactions': 'C=C.C1=CC(=CC(=C1)Cl)C(=O)OO>>O=C(O)c1cccc(Cl)c1.O1CC1'}]
+        expected_result = {'reactions': 'C=C.C1=CC(=CC(=C1)Cl)C(=O)OO>>O=C(O)c1cccc(Cl)c1.O1CC1'}  # No change
+
+        # Applying the fix method
+        self.pero_imputer.fix(reactions_dict[0], 'reactions')
+
+        # Assertion
+        self.assertEqual(reactions_dict[0], expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
