aa1999d5c977cac85459a3592365f5f009a54232
Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>
2024-01-10 11:44:32 -0500

    improve mcs impute tool

--- Pipeline/7_impute_on_mcs.py
+++ Pipeline/7_impute_on_mcs.py
@@ -1,8 +1,12 @@
+import os
 import argparse
+import collections
 
 from SynRBL.rsmi_utils import load_database, save_database
 from SynRBL.SynMCSImputer.rules import MergeRule, CompoundRule
 from SynRBL.SynMCSImputer.model import MCSImputer
+
+DATABASE_PATH = "./Data/Validation_set/"
 
 
 def print_rule_summary(rule_map):
@@ -29,9 +33,7 @@ def print_success_rate(dataset):
             success_cnt += 1
     print(
         "Reached carbon balance on {:.2%} ({}/{}) of the reactions.".format(
-            success_cnt / len(dataset),
-            success_cnt,
-            len(dataset)
+            success_cnt / len(dataset), success_cnt, len(dataset)
         )
     )
 
@@ -55,22 +57,72 @@ def impute_new_reactions(data):
 
 
 def get_database_path(dataset, name):
-    return "./Data/Validation_set/{}/MCS/{}.json.gz".format(dataset, name)
+    return os.path.join(DATABASE_PATH, dataset, "MCS", "{}.json.gz".format(name))
+
+
+def get_databases():
+    dbs = set()
+    dir_names = [f.name for f in os.scandir(DATABASE_PATH) if f.is_dir()]
+    for d in dir_names:
+        if os.path.exists(os.path.join(DATABASE_PATH, d, "MCS/Final_Graph.json.gz")):
+            dbs.add(d)
+        else:
+            print("[ERROR] Directory '{}' is not a valid database.".format(d))
+    return list(dbs)
+
+
+def run_impute(args):
+    data = load_database(get_database_path(args.dataset, "Final_Graph"))
+    rule_map = impute_new_reactions(data)
+    save_database(data, get_database_path(args.dataset, "MCS_Impute"))
+    print_rule_summary(rule_map)
+    print_success_rate(data)
+
+
+def run_report(args):
+    dbs = get_databases()
+    line_fmt = "{:<25} {:>12} {:>12} {:>9}"
+    header = line_fmt.format("Dataset", "Reactions", "C balanced", "Rate")
+    print(header)
+    print("-" * len(header))
+    for db in dbs:
+        path = get_database_path(db, "MCS_Impute")
+        if not os.path.exists(path):
+            print(
+                (
+                    "[ERROR] No MCS_Impute data found for dataset '{}'. "
+                    + "Run 'impute --dataset {}' first."
+                ).format(db, db)
+            )
+        data = load_database(path)
+        success_cnt = 0
+        for item in data:
+            if item["issue"] == "":
+                success_cnt += 1
+        rate_str = "{:.2%}".format(success_cnt / len(data))
+        print(line_fmt.format(db, len(data), success_cnt, rate_str))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="MCSImpute", description="Imputes new compounds based on the MCS results."
     )
-    parser.add_argument(
+    subparsers = parser.add_subparsers(dest="command")
+
+    impute_parser = subparsers.add_parser(
+        "impute", description="Impute compounds from MCS results."
+    )
+    impute_parser.add_argument(
         "--dataset",
         default="USPTO_test",
         help="The name of the dataset directory in ./Data/Validation_set/",
     )
-    parser.add_argument("--summary", action="store_true")
+    impute_parser.set_defaults(func=run_impute)
+
+    report_parser = subparsers.add_parser(
+        "report", description="Print summary on MCS imputation performance."
+    )
+    report_parser.set_defaults(func=run_report)
+
     args = parser.parse_args()
-    data = load_database(get_database_path(args.dataset, "Final_Graph"))
-    rule_map = impute_new_reactions(data)
-    save_database(data, get_database_path(args.dataset, "MCS_Impute"))
-    print_rule_summary(rule_map)
-    print_success_rate(data)
+    args.func(args)
