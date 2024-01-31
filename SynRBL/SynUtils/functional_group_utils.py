f16f0580332ddfa175613fba019b353ff0f85c6d
Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>
2024-01-31 17:18:12 +0100

    reimplement functional group checks

--- SynRBL/SynUtils/functional_group_utils.py
+++ SynRBL/SynUtils/functional_group_utils.py
@@ -1,3 +1,5 @@
+import copy
+import itertools
 import numpy as np
 import rdkit.Chem as Chem
 import rdkit.Chem.rdchem as rdchem
@@ -47,17 +49,21 @@ class FGConfig:
 
 
 functional_group_config = {
-    "phenol": FGConfig("Oc1ccccc1"),
+    "phenol": FGConfig(
+        ["Oc1ccccc1", "Oc1ccc[nH]1"], anti_pattern=["COc1ccccc1", "COc1ccc[nH]1"]
+    ),
     "alcohol": FGConfig(
-        "CO", anti_pattern=["C(=O)O", "C=CO", "COC", "OCO", "Oc1ccccc1"]
+        "CO", anti_pattern=["C(=O)O", "C=CO", "COC", "OCO", "Oc1ccccc1", "Oc1ccc[nH]1"]
     ),
     "ether": FGConfig(
-        "COC", group_atoms=[1], anti_pattern=["OC=S", "C=O", "C=CO", "OCOC", "OC=O", "OCN"]
+        "COC",
+        anti_pattern=["OC=S", "C=O", "C=CO", "OCOC", "OC=O", "OCN"],
     ),
     "enol": FGConfig("C=CO"),
     "amid": FGConfig("NC=O", anti_pattern=["O=C(N)O"]),
     "acyl": FGConfig(
-        "C=O", anti_pattern=["SC=O", "C(N)=O", "O=C(O)O", "O=C(C)OC", "O=C(C)O"]
+        "C=O",
+        anti_pattern=["SC=O", "C(N)=O", "O=C(O)O", "O=C(C)OC", "O=C(C)O", "CC(=O)OC=O"],
     ),
     "diol": FGConfig("OCO", anti_pattern=["OCOC", "O=C(O)O"]),
     "hemiacetal": FGConfig("COCO", anti_pattern=["COCOC", "O=C(O)O"]),
@@ -77,7 +83,7 @@ functional_group_config = {
     "hydroxylamin": FGConfig("NO", anti_pattern=["O=NO"]),
     "nitrose": FGConfig("N=O", anti_pattern=["O=NO"]),
     "nitro": FGConfig("O=NO"),
-    "thioether": FGConfig("CSC", group_atoms=[1], anti_pattern=["O=CS"]),
+    "thioether": FGConfig("CSC", anti_pattern=["O=CS"]),
     "thioester": FGConfig(["O=CS", "OC=S"]),
 }
 
@@ -106,6 +112,104 @@ def trim_mol(mol: rdchem.Mol, index: int, depth: int) -> rdchem.Mol:
     return rwmol, atom.GetIdx()
 
 
+def get_mapping_permutations(match_symbols, sym_dict):
+    mappings = []
+    if len(sym_dict) >= len(match_symbols):
+        sym_map = [(i, s) for i, s in enumerate(sym_dict)]
+        for sym_permut in itertools.permutations(sym_map):
+            mapping = []
+            is_match = True
+            for i, s1 in enumerate(match_symbols):
+                if s1 == sym_permut[i][1]:
+                    mapping.append((i, sym_permut[i][0]))
+                else:
+                    is_match = False
+                    break
+            if is_match:
+                mappings.append(mapping)
+    return mappings
+
+
+def pattern_match(mol, anchor, pattern_mol, pattern_anchor=None):
+    def _fits(atom, pattern_atom, visited_atoms=[], visited_pattern_atoms=[]):
+        atom_idx = atom.GetIdx()
+        pattern_atom_idx = pattern_atom.GetIdx()
+        assert atom_idx not in visited_atoms
+        assert pattern_atom_idx not in visited_pattern_atoms
+
+        visited_atoms = copy.deepcopy(visited_atoms)
+        visited_atoms.append(atom_idx)
+        visited_pattern_atoms = copy.deepcopy(visited_pattern_atoms)
+        visited_pattern_atoms.append(pattern_atom_idx)
+
+        atom_neighbors = [
+            (a.GetIdx(), a.GetSymbol(), a)
+            for a in atom.GetNeighbors()
+            if a.GetIdx() not in visited_atoms
+        ]
+        pattern_neighbors = [
+            (a.GetIdx(), a.GetSymbol(), a)
+            for a in pattern_atom.GetNeighbors()
+            if a.GetIdx() not in visited_pattern_atoms
+        ]
+
+        fits = False
+        match = []
+        if atom.GetSymbol() == pattern_atom.GetSymbol():
+            match.append((atom.GetIdx(), pattern_atom.GetIdx()))
+            if len(pattern_neighbors) > 0:
+                an_syms = [a[1] for a in atom_neighbors]
+                pn_syms = [a[1] for a in pattern_neighbors]
+                mappings = get_mapping_permutations(pn_syms, an_syms)
+                for mapping in mappings:
+                    valid_mapping = True
+                    n_matches = set()
+                    for pn_i, an_i in mapping:
+                        atom_neighbor = atom_neighbors[an_i]
+                        pattern_neighbor = pattern_neighbors[pn_i]
+                        mol = atom_neighbor[2].GetOwningMol()
+                        p_mol = pattern_neighbor[2].GetOwningMol()
+                        bond = mol.GetBondBetweenAtoms(
+                            atom_idx, atom_neighbor[0]
+                        ).GetBondType()
+                        p_bond = p_mol.GetBondBetweenAtoms(
+                            pattern_atom_idx, pattern_neighbor[0]
+                        ).GetBondType()
+                        if bond != p_bond:
+                            valid_mapping = False
+                            break
+                        n_fit, n_match = _fits(
+                            atom_neighbor[2],
+                            pattern_neighbor[2],
+                            visited_atoms,
+                            visited_pattern_atoms,
+                        )
+                        if not n_fit:
+                            valid_mapping = False
+                            break
+                        else:
+                            n_matches.update(n_match)
+                    if valid_mapping:
+                        fits = True
+                        match.extend(n_matches)
+                        break
+            else:
+                fits = True
+        return fits, match
+
+    if pattern_anchor is None:
+        atom = mol.GetAtomWithIdx(anchor)
+        for pattern_atom in pattern_mol.GetAtoms():
+            result = _fits(atom, pattern_atom)
+            if result[0]:
+                return result
+        return False, [[]]
+    else:
+        atom = mol.GetAtomWithIdx(anchor)
+        pattern_atom = pattern_mol.GetAtomWithIdx(pattern_anchor)
+        return _fits(atom, pattern_atom)
+
+
 def is_functional_group(mol: rdchem.Mol, group_name: str, index: int) -> bool:
     if group_name not in functional_group_config.keys():
         raise NotImplementedError(
@@ -115,12 +219,11 @@ def is_functional_group(mol: rdchem.Mol, group_name: str, index: int) -> bool:
 
     is_func_group = False
 
-    rmol, index = trim_mol(mol, index, config.max_pattern_size - 1)
     for p_mol, g_mol in zip(config.pattern, config.groups):
-        pattern_match = list(rmol.GetSubstructMatch(p_mol))
-        if index in pattern_match:
-            group_match = list(rmol.GetSubstructMatch(g_mol))
-            is_func_group = is_func_group or index in group_match
+        is_match, _ = pattern_match(mol, index, p_mol)
+        if is_match:
+            is_match, _ = pattern_match(mol, index, g_mol)
+            is_func_group = is_func_group or is_match
 
     last_len = config.max_pattern_size
     for ap_mol, ap_mol_size in sorted(
@@ -132,8 +235,7 @@ def is_functional_group(mol: rdchem.Mol, group_name: str, index: int) -> bool:
             break
         ap_mol_size = len(ap_mol.GetAtoms())
         if last_len > ap_mol_size:
-            rmol, index = trim_mol(rmol, index, ap_mol_size - 1)
             last_len = ap_mol_size
-        group_match = list(rmol.GetSubstructMatch(ap_mol))
-        is_func_group = is_func_group and index not in group_match
+        is_match, _ = pattern_match(mol, index, ap_mol)
+        is_func_group = is_func_group and not is_match
     return is_func_group
