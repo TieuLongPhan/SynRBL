4dc9bab5efe44a7f2d2b0da4e9f5c650af0fa4fd
Tieu Long Phan <ptlong.chcndpbc20@ump.edu.vn>
2024-03-21 10:15:13 +0100

    update readme

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version='0.1.1',
     description='Synthesis Rebalancing Framework for Computational Chemistry',
     author='TieuLongPhan',
-    author_email='your.email@example.com',
+    author_email='ptlong8995@gmail.com',
     url='https://github.com/TieuLongPhan/SynRBL',
     packages=find_packages(),
     install_requires=[
-        'rdkit',
+        'rdkit==2023.9.5',
         'joblib'
         # add other dependencies as needed
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.11',
     # Additional metadata like classifiers, keywords, etc.
 )
