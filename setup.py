2946c2f0f04a031dcbce67f37e4a57aa2239887e
TieuLongPhan <ptlong.chcndpbc20@ump.edu.vn>
2024-03-27 10:59:14 +0100

    update notebook

--- setup.py
+++ setup.py
@@ -9,10 +9,21 @@ setup(
     url='https://github.com/TieuLongPhan/SynRBL',
     packages=find_packages(),
     install_requires=[
-        'rdkit',
-        'joblib'
-        # add other dependencies as needed
+        'rdkit==2023.9.4',
+        'joblib==1.3.2',
+        'seaborn==0.13.2',
+        'xgoost==2.0.3',
+        'scikit_learn==1.4.1.post1',
+        'imbalanced_learn==0.12.0',
+        'reportlab==4.1.0'
+ 
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.11',
     # Additional metadata like classifiers, keywords, etc.
 )
+
+
+
+
+
+
