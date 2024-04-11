8e22a701331e0c7c6e86194d7d213644d8f088a8
Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>
2024-04-11 15:03:47 +0200

    add github actions

--- lint_check.sh
+++ lint_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flake8 . --count --exit-zero --max-complexity=13 --max-line-length=90 \
+flake8 . --count --max-complexity=13 --max-line-length=90 \
 	--per-file-ignores="__init__.py:F401 \
 		mcs_graph_detector.py:C901 \
 		eda_analysis.py:C901 \
