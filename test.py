c88ec9fd6d755954c91b9c74a730b01b9fcc7d72
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2024-04-15 23:22:03 +0200

    add test case

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from synrbl import Balancer
 from synrbl.SynUtils import normalize_smiles
 
-smiles = "[NH3+]C(CCC=O)C(=O)[O-]>>O=C([O-])C1CCC=N1"
+smiles = "Nc1nc2c(ncn2C2OC(COP(=O)([O-])OP(=O)([O-])OP(=O)([O-])[O-])C(O)C2O)c(=O)[nH]1.O>>Nc1nc2c(ncn2C2OC(COP(=O)([O-])[O-])C(O)C2O)c(=O)[nH]1"
+print(normalize_smiles(smiles))
 balancer = Balancer()
 result = balancer.rebalance(smiles, output_dict=True)
 print(result)
