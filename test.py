8ef71b8e180ac2f51ce077835ab04c0d3dce82b9
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2024-04-17 00:30:25 +0200

    remove debug file

--- test.py
+++
@@ -1,8 +0,0 @@
-from synrbl import Balancer
-from synrbl.SynUtils import normalize_smiles
-
-smiles = "Nc1nc2c(ncn2C2OC(COP(=O)([O-])OP(=O)([O-])OP(=O)([O-])[O-])C(O)C2O)c(=O)[nH]1.O>>Nc1nc2c(ncn2C2OC(COP(=O)([O-])[O-])C(O)C2O)c(=O)[nH]1"
-print(normalize_smiles(smiles))
-balancer = Balancer()
-result = balancer.rebalance(smiles, output_dict=True)
-print(result)
