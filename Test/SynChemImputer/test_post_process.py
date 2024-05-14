b05f6429fcd42ef29c267a88935b101ae9162d55
Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>
2024-05-14 15:07:49 +0200

    fix post process arguments

--- Test/SynChemImputer/test_post_process.py
+++ Test/SynChemImputer/test_post_process.py
@@ -20,8 +20,7 @@ class TestPostProcess(unittest.TestCase):
         ]
 
     def test_label_reactions(self):
-
-        post_process = PostProcess(self.data)
+        post_process = PostProcess()
         result = post_process.label_reactions(self.data[0])
         self.assertEqual(result["label"], "Reduction")
         result = post_process.label_reactions(self.data[1])
@@ -30,13 +29,13 @@ class TestPostProcess(unittest.TestCase):
         self.assertEqual(result["label"], "unspecified")
 
     def test_fit(self):
-        post_process = PostProcess(self.data)
-        result = post_process.fit(n_jobs=4, verbose=1)
+        post_process = PostProcess(n_jobs=4, verbose=1)
+        result = post_process.fit(self.data)
         self.assertEqual(result[1]["curated_reaction"], "CC(=O)C.[HH]>>CC(O)C")
         self.assertEqual(
             result[2]["curated_reaction"],
             (
-                "CCO.O=[Cr](Cl)(-[O-])=O.c1cc[nH+]cc1>>"
+              "CCO.O=[Cr](Cl)(-[O-])=O.c1cc[nH+]cc1>>"
                 + "CC=O.O=[Cr](O)O.c1cc[nH+]cc1.[Cl-]"
             ),
         )
