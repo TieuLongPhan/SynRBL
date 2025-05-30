dd72585c02e7fc28d1158cdf5278495a76e5121a
TieuLongPhan <ptlong.chcndpbc20@ump.edu.vn>
2025-05-30 15:30:08 +0200

    fix lint

--- lint.sh
+++ lint.sh
@@ -4,6 +4,7 @@ flake8 . --count --max-complexity=13 --max-line-length=90 \
 	--per-file-ignores="__init__.py:F401 \
 		mcs_graph_detector.py:C901 \
 		eda_analysis.py:C901 \
-		find_missing_graphs.py:C901" \
+		find_missing_graphs.py:C901 \
+		validation_set_interface.py:F824" \
 	--exclude venv \
 	--statistics
